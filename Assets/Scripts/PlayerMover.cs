c5b9b67dcef755b2745ea09a643dccbd61b60970
PalverZ <Palverz@yahoo.com>
2018-07-21 23:33:39 -0600

    Initial commit

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour
+{
+    [SerializeField] private float travelSpeed = 1f;
+    [SerializeField] private float distanceTolerance = .01f;
+
+    private Vector3 orginVector3;
+    private Transform destinationMarker;
+    private float startMoveTime;
+    private float distanceOfTravel;
+
+    private void Awake()
+    {
+        TravelNode.OnNavigationInteract += HandleNavigationInteract;
+    }
+
+    private void HandleNavigationInteract(TravelNode node)
+    {
+        SetTravelLocation(node.transform);
+    }
+
+    private void SetTravelLocation(Transform transform)
+    {
+        orginVector3 = transform.position;
+        destinationMarker = transform;
+        startMoveTime = Time.time;
+        distanceOfTravel = Vector3.Distance(orginVector3, destinationMarker.position);
+    }
+
+    private void Update()
+    {
+        if (destinationMarker == null) return;
+
+        DoTheMove(Time.deltaTime);
+        if (CloseEnoughToDestination()) destinationMarker = null;
+    }
+
+    private void DoTheMove(float deltaTime)
+    {
+        transform.LookAt(destinationMarker);
+       transform.position += transform.forward * deltaTime * travelSpeed;
+    }
+
+    private bool CloseEnoughToDestination()
+    {
+        return Vector3.Distance(transform.position, destinationMarker.position) <= distanceTolerance;
+    }
+}
---
+++ Assets/Scripts/TravelNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+public class TravelNode : MonoBehaviour
+{
+    public static event Action<TravelNode> OnNavigationInteract;
+
+    public void FunctionToDoWhenImInteractedWith()
+    {
+        OnNavigationInteract(this);
+    }
+
+
+    //When object is clicked (Change this to what you need
+    public void OnMouseDown()
+    {
+        FunctionToDoWhenImInteractedWith();
+    }
+
+}
